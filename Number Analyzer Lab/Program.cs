8e96e56633b84d17fe79611c58cf48cb0a1fe763
KARBIDE17 <stevewoods17@gmail.com>
2023-01-13 12:49:36 -0500

    added test for valid input, changed block of if statements to else if chain, fixed response for 100 as input

--- Number Analyzer Lab/Program.cs
+++ Number Analyzer Lab/Program.cs
@@ -3,53 +3,54 @@
 
 Console.WriteLine("What is Your Name?");  //  Ask for user information (ex. name) at the beginning of the application, and use it to refer to the user throughout the application.
 string name = Console.ReadLine();
-
+bool isValidNum;
 do
 {
-
-    
     Console.WriteLine("Hey " + name + "! Please enter a number between 1 an 100..."); ////prompts the user to enter an integer between 1 and 100
-    int input = int.Parse(Console.ReadLine());
+    string input = Console.ReadLine();
+    isValidNum = int.TryParse(input, out int num);
    
-    Console.WriteLine("You entered: " + input);//Display the associated result based on the integer range entered
-
-    if (input % 2 == 1 && input < 60) //If the integer entered is odd and less than 60, print the number entered and “Odd and less than 60.”
+    if (isValidNum == false)
     {
-        Console.WriteLine(name +"... the number you entered is Odd and less than 60");
+        Console.WriteLine("Please enter only numbers \n");
     }
 
-    if (input % 2 == 0 && input >= 2 && input <= 24) //If the integer entered is even and in the inclusive range of 2 to 24, print “Even and less than 25.”
+    if (isValidNum == true)
     {
-        Console.WriteLine(name + "... the number you entered is Even and less than 25.");
+        Console.WriteLine("You entered: " + num + "\n");//Display the associated result based on the integer range entered
+        if (num > 100)
+        {
+            Console.WriteLine("Sorry " + name + "... that number is too large for this program.");
+        }
+        else if (num < 1)
+        {
+            Console.WriteLine("Sorry " + name + "... that number is too small for this program.");
+        }
+        else if (num % 2 == 1 && num < 60) //If the integer entered is odd and less than 60, print the number entered and “Odd and less than 60.”
+        {
+            Console.WriteLine(name + "... the number you entered is Odd and less than 60");
+        }
+        else if (num % 2 == 0 && num >= 2 && num <= 24) //If the integer entered is even and in the inclusive range of 2 to 24, print “Even and less than 25.”
+        {
+            Console.WriteLine(name + "... the number you entered is Even and less than 25.");
+        }
+        else if (num % 2 == 0 && num >= 26 && num <= 60) //If the integer entered is even and in the inclusive range of 26 to 60, print “Even and between 26 and 60 inclusive.”
+        {
+            Console.WriteLine(name + "... the number you entered is Even and between 26 and 60 inclusive.");
+        }
+        else if (num % 2 == 0 && num > 60 && num <= 100)  //If the integer entered is even and greater than 60, print the number entered and “Even and greater than 60.”
+        {
+            Console.WriteLine(name + "... the number you entered is Even and greater than 60");
+        }
+        else if (num % 2 == 1 && num > 60 && num < 100)
+        {
+            Console.WriteLine(name + "... the number you entered is Odd and greater than 60");
+        }
     }
-
-    if (input % 2 == 0 && input >= 26 && input <= 60) //If the integer entered is even and in the inclusive range of 26 to 60, print “Even and between 26 and 60 inclusive.”
-    {
-        Console.WriteLine(name + "... the number you entered is Even and between 26 and 60 inclusive.");
-    }
-
-    if (input % 2 == 0 && input > 60 && input < 100)  //If the integer entered is even and greater than 60, print the number entered and “Even and greater than 60.”
-    {
-        Console.WriteLine(name + "... the number you entered is Even and greater than 60");
-    }
-
-    if (input % 2 == 1 && input > 60 && input < 100)
-    {
-        Console.WriteLine(name + "... the number you entered is Odd and greater than 60");
-    }
-
-    if (input > 100)
-    {
-        Console.WriteLine("Sorry " + name + "... that number is too large for this program.");
-    }
-
-    if (input < 1)
-    {
-        Console.WriteLine("Sorry " + name + "... that number is too small for this program.");
-    }
-    Console.WriteLine(name + ", would you like to continue? y/n?"); //continue running with a prompt at the end to see if they want to stop. 
-    string choice2 = Console.ReadLine();
-    while (choice2 != "y")
+    Console.WriteLine("\n" + name + ", would you like to continue? y/n?"); //continue running with a prompt at the end to see if they want to stop. 
+    string input2 = Console.ReadLine();
+    
+    while (input2 != "y")
     {
         Console.WriteLine("See Ya!");
         Environment.Exit(0);
